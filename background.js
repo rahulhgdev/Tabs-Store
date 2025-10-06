597dd271e16318f232dce2c3b26e1e826fa46acd
rahulhgdev <rahul.gupta7749@gmail.com>
2025-10-06 22:47:45 +0530

    Enhance tab saving functionality to create unique names for quick saves

--- background.js
+++ background.js
@@ -4,9 +4,14 @@ chrome.commands.onCommand.addListener(function(command) {
       const urlsToAdd = tabs.map(tab => tab.url).filter(Boolean);
       const createdDate = new Date();
       const formattedDate = `${createdDate.getDate()}/${createdDate.getMonth() + 1}/${createdDate.getFullYear()}`;
+
       chrome.storage.local.get({ accordions: [] }, (result) => {
         const accordions = result.accordions || [];
-        accordions.push({ name: "Quick Save", date: formattedDate, urls: urlsToAdd });
+        const quickSaveCount = accordions.filter(item =>
+          item.name.startsWith("Quick Save")
+        ).length;
+        const uniqueName = quickSaveCount === 0 ? "Quick Save_1" : `Quick Save_${quickSaveCount + 1}`;
+        accordions.push({ name: uniqueName, date: formattedDate, urls: urlsToAdd });
         chrome.storage.local.set({ accordions });
       });
     });
