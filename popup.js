089a16de650d8314532be93754b87a95faddf0aa
rahulhgdev <rahul.gupta7749@gmail.com>
2025-10-18 11:40:32 +0530

    Update popup interface to include 'Extract from text' button and improve button labels; implement URL extraction functionality in popup.js for enhanced user experience.

--- popup.js
+++ popup.js
@@ -4,6 +4,7 @@ const clearTabs = document.getElementById("clearBtn");
 const tabList = document.getElementById("urlList");
 const multiUrlsTextarea = document.getElementById("multiUrls"); // #tab2
 const openMultiUrlsButton = document.getElementById("openMultiUrls");
+const extractURLs = document.getElementById("extractURLs");
 const getPreserve = document.getElementById("preserveUrls");
 const deleteMultiUrlsButton = document.getElementById("deleteMultiUrls");
 const openInGroup = document.getElementById("openInGroup");
@@ -394,6 +395,20 @@ openMultiUrlsButton.addEventListener("click", () => {
     }
   }
 });
+
+// Extract urls from text
+extractURLs.addEventListener("click", ()=> {
+  let text = multiUrlsTextarea.value;
+  if (text.length === 0) {
+    alert("Please enter URLs!");
+    return;
+  }
+  let regexToMatch = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+  let matches = text.match(regexToMatch);
+  if(matches){
+    multiUrlsTextarea.value = matches.join('\n');
+  }
+})
 
 // Delete all URLs from textarea and storage
 deleteMultiUrlsButton.addEventListener("click", () => {
